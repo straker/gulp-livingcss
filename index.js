336fa37d77723ea2cbd71b699a7fa07619f1f03a
Alexander Vassbotn Røyne-Helgesen <alexander@phun-ky.net>
2019-06-28 08:50:43 +0200

    Only use level 1 + dedupe

--- index.js
+++ index.js
@@ -94,7 +94,13 @@ module.exports = function (dest, options) {
                 return stylesheet;
               })
               .join('\n');
-            var allDedupedCSS =  new CleanCSS({ level: 2 })
+            var allDedupedCSS =  new CleanCSS({
+              level: {
+                2: {
+                  all: false, // sets all values to `false`
+                  removeDuplicateRules: true // turns on removing duplicate rules
+                }
+              }})
               .minify(allCSS)
               context.parsedStylesheets = [allDedupedCSS.styles];;
           }
